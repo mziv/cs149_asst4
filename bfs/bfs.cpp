c616216501b730e582f2a75c2f33a5477a55b1d9
Maya Ziv <mziv@stanford.edu>
2020-11-09 22:19:32 -0800

    loop over everything 2

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -139,14 +139,15 @@ void bottom_up_step(
         vertex_set partial_frontier;
         vertex_set_init(&partial_frontier, g->num_nodes);
         
-        vertex_set partial_unvisited;
-        vertex_set_init(&partial_unvisited, g->num_nodes);
+        // vertex_set partial_unvisited;
+        // vertex_set_init(&partial_unvisited, g->num_nodes);
 
         // if v has not been visited 
         // double start_time = CycleTimer::currentSeconds();
         #pragma omp for 
-        for (int i = 0; i < unvisited->count; ++i) {
-            int v = unvisited->vertices[i];            
+        for (int i = 0; i < g->num_nodes; ++i) {
+            int v = i; //unvisited->vertices[i];    
+            if (distances[v] != NOT_VISITED_MARKER) continue;        
             // check if v shares an incoming edge with a vertex u on the frontier
             bool shares_edge = false;
             int start_edge = g->incoming_starts[v];
@@ -167,7 +168,7 @@ void bottom_up_step(
                 distances[v] = cur_dist;
             } else {
                 // v is still unvisited
-                partial_unvisited.vertices[partial_unvisited.count++] = v;
+                // partial_unvisited.vertices[partial_unvisited.count++] = v;
             }
         }
         // double end_time = CycleTimer::currentSeconds();
@@ -183,8 +184,8 @@ void bottom_up_step(
         //     new_frontier->vertices[i + index] = partial_frontier[i];
         // }
 
-        index = __sync_fetch_and_add(&new_unvisited->count, partial_unvisited.count);
-        memcpy(new_unvisited->vertices + index, (partial_unvisited.vertices), sizeof(int)*partial_unvisited.count);
+        // index = __sync_fetch_and_add(&new_unvisited->count, partial_unvisited.count);
+        // memcpy(new_unvisited->vertices + index, (partial_unvisited.vertices), sizeof(int)*partial_unvisited.count);
         // #pragma omp parallel for                                                   
         // for (int i = 0; i < partial_unvisited.size(); ++i) {            
         //     new_unvisited->vertices[i + index] = partial_unvisited[i];
